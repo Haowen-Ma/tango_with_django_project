9bc756fc5605eccd1b09d683c02bc54fd2b5ac25
Haowen Ma <2960966M@student.gla.ac.uk>
2025-02-13 04:00:55 +0800

    eighth commit2

--- rango/views.py
+++ rango/views.py
@@ -44,7 +44,7 @@ def show_category(request, category_name_slug):
         context_dict['category'] = None
         context_dict['pages'] = None
     return render(request, 'rango/category.html', context=context_dict)      
-
+@login_required
 def add_category(request):
     form = CategoryForm() 
     if request.method == 'POST': 
@@ -56,7 +56,7 @@ def add_category(request):
         else:
             print(form.errors)
     return render(request, 'rango/add_category.html', {'form': form})    
-
+@login_required
 def add_page(request, category_name_slug):
     category = Category.objects.filter(slug=category_name_slug).first()
     
@@ -120,7 +120,7 @@ def user_login(request):
 
 @login_required 
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")       
+    return render(request, 'rango/restricted.html', {})      
 @login_required 
 def user_logout(request):
     logout(request)
