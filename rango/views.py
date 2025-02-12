b02fb30637e64618ca196215358cc81160295525
Haowen Ma <2960966M@student.gla.ac.uk>
2025-02-13 05:20:35 +0800

    ninth commit2

--- rango/views.py
+++ rango/views.py
@@ -21,20 +21,25 @@ def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     
-    context_dict = {}
+    context_dict = {
+        'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!',
+        'categories': category_list,
+        'pages': page_list,
+    }
+
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
     visitor_cookie_handler(request)
-    context_dict['visits'] = request.session['visits']
+    
     response = render(request, 'rango/index.html', context=context_dict)
     return response
     
 def about(request):
-        
-        print(request.method)
-        print(request.user)
-        return render(request, 'rango/about.html', {})
+        visitor_cookie_handler(request)
+        visits = request.session.get('visits', 1)
+        context_dict = {'visits': visits}
+        return render(request, 'rango/about.html', context=context_dict)
 
 def show_category(request, category_name_slug):
     context_dict = {}
